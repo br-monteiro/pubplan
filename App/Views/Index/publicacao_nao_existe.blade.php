9e851c3525255fd734c3cd18a036d8d24bafb762
br-monteiro <bruno.monteirodg@gmail.com>
2016-10-26 11:31:40 -0300

    adicionado a mensagem de informação de falha ao localizar a publicação

--- App/Views/Index/publicacao_nao_existe.blade.php
+++ App/Views/Index/publicacao_nao_existe.blade.php
@@ -11,43 +11,11 @@
     <div class="row row-offcanvas row-offcanvas-right">
 
         <div class="col-xs-12 col-sm-9">
-
-            <div class="page-header">
-                <h1>{{$resultPublicacao['titulo']}}</h1>
+            <div class="alert alert-danger">
+                <h2>
+                    <i class="fa fa-meh-o"></i> Não foi possível localizar a publicação ...
+                </h2>
             </div>
-            <div class="row">
-                <div class="col-xs-6 col-md-4 text-center">
-                    <div class="thumbnail">
-                        <img src="{{APPDIR}}images/uploads/{{$resultPublicacao['id']}}.jpg" alt="" width="235" height="300">
-                        <div class="btn-group" style="margin-top: 10px;" role="group" aria-label="selecione...">
-                            <a href="{{APPDIR}}" class="btn btn-primary">
-                                <i class="fa fa-mail-reply"></i> Retornar
-                            </a>
-                            <a href="{{$resultPublicacao['link'] or APPDIR . 'files_uploads/' . $resultPublicacao['id'] . '.pdf'}}" class="btn btn-warning">
-                                <i class="fa fa-book"></i> Ler Publicação
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xs-6 col-md-6 ">
-                    <label>Ano</label>
-                    <p style="text-justify">{{$resultPublicacao['ano']}}</p>
-                    <label>Editora</label>
-                    <p style="text-justify">{{$resultPublicacao['editora']}}</p>
-                    <label>Edição</label>
-                    <p style="text-justify">{{$resultPublicacao['edicao']}}</p>
-                    <label>ISBN</label>
-                    <p style="text-justify">{{$resultPublicacao['isbn']}}</p>
-                    <label>Quantidade de Páginas</label>
-                    <p style="text-justify">{{$resultPublicacao['numero_pagias']}}</p>
-                    <label>Palavras-Chaves</label>
-                    <p style="text-justify">{{$resultPublicacao['palavras_chave']}}</p>
-                </div>
-            </div>
-            <label>Sinopse</label>
-            <p class="text-justify">
-                {{$resultPublicacao['sinopse']}}
-            </p>
         </div><!--/.col-xs-12.col-sm-9-->
 
         @include('Index.lista_categorias')
