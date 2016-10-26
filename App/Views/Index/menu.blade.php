49e04289a731617da01d82d4b78b7d9769d9b89f
Paulo Henrique <phcgaia11@yahoo.com.br>
2016-10-26 09:16:49 -0300

    Migrado o menu para um arquivo único e importado nas telas necessárias. Criado o método do Carousel para trazer os livros de melhor ranking em um mês e criado um método para limitar o número de livros de publicações na home do sistema.

---
+++ App/Views/Index/menu.blade.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-fixed-top navbar-inverse">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Publicações Seplan</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="{{APPDIR}}">Home</a></li>
+                <li><a href="#about">Seplan</a></li>
+                <li><a href="#contact">Contatos</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="{{APPDIR}}publicacoes/"><i class="fa fa-lock"></i> Login</a></li>
+            </ul>
+        </div><!-- /.nav-collapse -->
+    </div><!-- /.container -->
+</nav><!-- /.navbar -->
