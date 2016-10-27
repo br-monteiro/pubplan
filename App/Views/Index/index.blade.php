e1464ed8f4d992b6370b43b0d234349691e4f805
Paulo Henrique <phcgaia11@yahoo.com.br>
2016-10-27 11:05:41 -0300

    Adicionado o campo de pesquisa na barra do menu

--- App/Views/Index/index.blade.php
+++ App/Views/Index/index.blade.php
@@ -11,7 +11,6 @@
     <div class="row row-offcanvas row-offcanvas-right">
 
         <div class="col-xs-12 col-sm-9">
-
             <!--Meu Carousel-->
             <div id="myCarousel" class="carousel slide" data-ride="carousel">
 
@@ -48,47 +47,9 @@
                     <span class="sr-only">Next</span>
                 </a>
             </div><!-- /.carousel -->
-
-            <?php
-            $i = 0;
-            $qtdPublicacaoes = 0;
-            $contador = 0;
-            $qtdPublicacaoes = count($resultPublicacoes);
-
-            foreach ($resultPublicacoes as $value):
-
-                //$modPulicacacoes = $qtdPublicacaoes % 4;
-                ++$contador;
-
-                if ($i == 0) {
-                    echo "<div class=\"row\">";
-                }
-                $i++;
-                ?>
-                <div class="col-sm-6 col-md-3">
-                    <div class="thumbnail">
-                        <a href="{{APPDIR}}index/detalhes/id/{{$value['id']}}/titulo/{{$value['titulo']}}">
-                            <img src="images/uploads/{{$value['id']}}.jpg" alt="{{$value['titulo']}}" width="100" height="100">
-                        </a>
-                    </div>
-                    <div class="caption">
-                        <a href="{{APPDIR}}index/detalhes/id/{{$value['id']}}/titulo/{{$value['titulo']}}">
-                            <h4>{{$value['titulo']}}</h4>
-                        </a>
-                    </div>
-                </div>
-                <?php
-                if ($contador == $qtdPublicacaoes) {
-                    echo "</div>";
-                    break;
-                }
-                if ($i == 4) {
-                    echo "</div>";
-                    $i = 0;
-                }
-            endforeach;
-            ?>
-
+            
+            <div class="lista-resultado"></div>
+            
         </div><!--/.col-xs-12.col-sm-9-->
         @include('layout.Partials.lista_categorias')
     </div><!--/row-->
